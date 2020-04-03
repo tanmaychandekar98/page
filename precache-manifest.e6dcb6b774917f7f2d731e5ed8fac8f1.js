733b3c1c77c2e721d6e20d564f627d2075d3cd55
tchandek <tchandek@cisco.com>
2020-04-03 12:16:02 +0530

    Updates

---
+++ precache-manifest.e6dcb6b774917f7f2d731e5ed8fac8f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25738e334c9210d4f906d00a8892339",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "b10d55a3fb38a48e2b8b",
+    "url": "/page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2cebfdaa4efa415103dc",
+    "url": "/page/static/js/2.fda07acf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/page/static/js/2.fda07acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10d55a3fb38a48e2b8b",
+    "url": "/page/static/js/main.2bf49c76.chunk.js"
+  },
+  {
+    "revision": "4fd3297acd4b36fb747d",
+    "url": "/page/static/js/runtime-main.d66b9751.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/page/static/media/logo.5d5d9eef.svg"
+  }
+]);
